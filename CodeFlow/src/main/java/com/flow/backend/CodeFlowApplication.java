8a30f98b15b4b5a6106d4aef27f102469763c9c2
morigamer845 <morielenriquesolises@gmail.com>
2026-08-11 19:09:08 -0600

    Creacion del poryecto
    la creacion del proyecto spring boot + Maven

---
+++ CodeFlow/src/main/java/com/flow/backend/CodeFlowApplication.java
@@ -0,0 +1,13 @@
+package com.flow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeFlowApplication.class, args);
+    }
+
+}
---
+++ CodeFlow/src/test/java/com/flow/backend/CodeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
